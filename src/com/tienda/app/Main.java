66875cdbc6c0b7bd5360f102aecc710b9488187b
AngieNatalia01 <angie123nata@gmail.com>
2026-09-14 00:47:17 -0500

    Create Main.java

---
+++ src/com/tienda/app/Main.java
@@ -0,0 +1,14 @@
+package com.tienda.app;
+
+import com.tienda.ui.MenuPrincipal;
+
+/**
+ * Clase principal - Punto de entrada de la aplicación
+ * Tienda Escolar - Aplicativo de Consola
+ */
+public class Main {
+    public static void main(String[] args) {
+        MenuPrincipal menu = new MenuPrincipal();
+        menu.ejecutar();
+    }
+}
